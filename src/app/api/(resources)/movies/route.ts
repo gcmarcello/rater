0f437d6f6dd84ccf7c37f06339c230c136794f5e
gcmarcello <marcellogcoelho@outlook.com>
2024-08-09 17:43:42 +0000

    chore: fixed imports

--- src/app/api/(resources)/movies/route.ts
+++ src/app/api/(resources)/movies/route.ts
@@ -1,5 +1,8 @@
+import { NextRequest } from "next/server";
 import { MovieService } from "./service";
 import { ServerResponse } from "@/app/api/classes/ServerResponse";
+import { ParsedRequest } from "../../../types/ParsedRequest";
+import { Prisma } from "@prisma/client";
 
 export async function GET() {
   try {
