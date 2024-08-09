84f068fe1660ea162f341a701ff44675e073fa21
gcmarcello <marcellogcoelho@outlook.com>
2024-08-09 14:50:05 +0000

    feat: Add Header component for displaying the website header

---
+++ src/app/(frontend)/components/Header.tsx
@@ -0,0 +1,20 @@
+"use client";
+import styled from "styled-components";
+import Button from "./Button";
+
+const HeaderContainer = styled.div`
+  width: 100dvw;
+
+  display: flex;
+  justify-content: space-between;
+  padding: 24px;
+`;
+
+export default function Header() {
+  return (
+    <HeaderContainer>
+      <img src="./Logo.png" alt="Logo" />
+      <Button variant="secondary">Login</Button>
+    </HeaderContainer>
+  );
+}
