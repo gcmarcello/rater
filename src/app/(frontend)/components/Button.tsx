6f5a44d2140bdec3b2623e7aa58eed1c49f52152
gcmarcello <marcellogcoelho@outlook.com>
2024-08-09 14:50:13 +0000

    feat: Add Button component for displaying styled buttons

---
+++ src/app/(frontend)/components/Button.tsx
@@ -0,0 +1,47 @@
+"use client";
+import React from "react";
+import styled from "styled-components";
+
+const BaselineButton = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 8px 28px;
+  margin-top: auto;
+  margin-bottom: auto;
+  gap: 8px;
+  border-radius: 12px;
+  cursor: pointer;
+  font-weight: 600;
+`;
+
+const PrimaryWhiteButton = styled(BaselineButton)`
+  background: #eeeeee;
+`;
+
+const SecondaryButton = styled(BaselineButton)`
+  background: #232323;
+  &:hover {
+    background: #333333;
+  }
+`;
+
+const TertiaryButton = styled(BaselineButton)``;
+
+export default function Button({
+  children,
+  variant,
+}: {
+  children: React.ReactNode;
+  variant: "primary" | "secondary" | "tertiary";
+}) {
+  switch (variant) {
+    case "primary":
+      return <PrimaryWhiteButton>{children}</PrimaryWhiteButton>;
+    case "secondary":
+      return <SecondaryButton>{children}</SecondaryButton>;
+    case "tertiary":
+      return <TertiaryButton>{children}</TertiaryButton>;
+  }
+}
