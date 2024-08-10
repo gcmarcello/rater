80f899a8aaf26142fcfea310bf04c964937f653f
gcmarcello <marcellogcoelho@outlook.com>
2024-08-10 18:37:34 +0000

    feat: added anchor links

--- src/app/(frontend)/_shared/components/Text.tsx
+++ src/app/(frontend)/_shared/components/Text.tsx
@@ -1,26 +1,44 @@
+import React, { AnchorHTMLAttributes } from "react";
 import styled from "styled-components";
 
-const WhiteText = styled.p`
+type TextProps = {
+  variant?: "white" | "dark";
+  children: React.ReactNode;
+} & AnchorHTMLAttributes<HTMLAnchorElement>;
+
+const WhiteText = styled.p<TextProps>`
   color: white;
   font-weight: 600;
 `;
 
-const DarkText = styled.p`
+const DarkText = styled.p<TextProps>`
   color: #121212;
   font-weight: 600;
 `;
 
-export default function Text({
-  children,
-  variant = "white",
-}: {
-  children: React.ReactNode;
-  variant: "white" | "dark";
-}) {
-  switch (variant) {
+const WhiteAnchor = styled.a<TextProps>`
+  color: white;
+  font-weight: 600;
+`;
+
+const DarkAnchor = styled.a<TextProps>`
+  color: #121212;
+  font-weight: 600;
+`;
+
+export default function Text(props: TextProps) {
+  switch (props.variant) {
     case "white":
-      return <WhiteText>{children}</WhiteText>;
+      return props.href ? (
+        <WhiteAnchor {...props}>{props.children}</WhiteAnchor>
+      ) : (
+        <WhiteText>{props.children}</WhiteText>
+      );
     case "dark":
-      return <DarkText>{children}</DarkText>;
+      return props.href ? (
+        <DarkAnchor {...props}>{props.children}</DarkAnchor>
+      ) : (
+        <DarkText>{props.children}</DarkText>
+      );
   }
 }
