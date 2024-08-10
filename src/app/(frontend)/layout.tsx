aee3d05edd4b7e6e86ea3603e361e8b579a58a2f
gcmarcello <marcellogcoelho@outlook.com>
2024-08-10 18:46:10 +0000

    dev: installed react hot toast

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/app/(frontend)/components/Header";
 import StyledComponentsRegistry from "../libs/styled-components/registry";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <StyledComponentsRegistry>
           <Header />
           {children}
+          <Toaster />
         </StyledComponentsRegistry>
       </body>
     </html>
