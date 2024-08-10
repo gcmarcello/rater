24ba11365aac39352e93a3fcc6f16e795540bdfc
gcmarcello <marcellogcoelho@outlook.com>
2024-08-10 23:06:29 +0000

    feat: auth decorator

---
+++ src/app/api/decorators/Authentication.ts
@@ -0,0 +1,48 @@
+import { ParsedRequest, ParsedRequestWithUser } from "@/app/types/Request";
+import { ServerResponse } from "../classes/ServerResponse";
+import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
+import { Session } from "@/app/types/Session";
+
+export function Authentication<T>() {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: TypedPropertyDescriptor<
+      (request: ParsedRequestWithUser<T>) => Promise<any>
+    >
+  ) {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = async function (request: ParsedRequestWithUser<T>) {
+      const token = cookies().get("token")?.value;
+
+      if (!token) {
+        throw {
+          message: "Usuário não autenticado.",
+          status: 401,
+        };
+      }
+
+      let newRequest = request as ParsedRequestWithUser<T>;
+
+      try {
+        const decodedToken = jwt.verify(
+          token,
+          process.env.JWT_SECRET!
+        ) as Session;
+        newRequest.user = decodedToken;
+      } catch (error) {
+        throw {
+          message: "Erro ao autenticar usuário.",
+          error,
+          status: 401,
+        };
+      }
+
+      return await originalMethod!.apply(this, [newRequest]);
+    };
+
+    return descriptor;
+  };
+}
