b5854ff42921b2914d4ac6bb791e9fd677b82027
gcmarcello <marcellogcoelho@outlook.com>
2024-08-11 02:24:39 +0000

    feat: Update Button component to include a danger variant

--- src/app/(frontend)/_shared/components/Button.tsx
+++ src/app/(frontend)/_shared/components/Button.tsx
@@ -1,14 +1,15 @@
 "use client";
+import { Button as HeadlessButton } from "@headlessui/react";
 import React from "react";
 import styled from "styled-components";
 
-const BaselineButton = styled.button`
+const BaselineButton = styled(HeadlessButton)`
   display: flex;
   font-family: "Inter", sans-serif;
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  padding: 8px 28px;
+  padding: 12px 28px;
   gap: 8px;
   border-radius: 12px;
   font-size: 16px;
@@ -25,6 +26,13 @@ const BaselineButton = styled.button`
 
 const PrimaryWhiteButton = styled(BaselineButton)`
   background: #eeeeee;
+  color: #232323;
+
+  &:hover {
+    &:not(:disabled) {
+      background: #cecece;
+    }
+  }
 `;
 
 const SecondaryButton = styled(BaselineButton)`
@@ -40,8 +48,13 @@ const TertiaryButton = styled(BaselineButton)`
   background: rgba(255, 255, 255, 0.2);
 `;
 
+const DangerButton = styled(BaselineButton)`
+  background: rgba(61, 16, 21, 1);
+  color: rgba(225, 29, 72, 1);
+`;
+
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "primary" | "secondary" | "tertiary";
+  variant?: "primary" | "secondary" | "tertiary" | "danger";
   children: React.ReactNode | string;
 }
 
@@ -55,6 +68,8 @@ export default function Button(props: ButtonProps) {
       return <SecondaryButton {...props}>{props.children}</SecondaryButton>;
     case "tertiary":
       return <TertiaryButton {...props}>{props.children}</TertiaryButton>;
+    case "danger":
+      return <DangerButton {...props}>{props.children}</DangerButton>;
     default:
       return (
         <PrimaryWhiteButton {...props}>{props.children}</PrimaryWhiteButton>
