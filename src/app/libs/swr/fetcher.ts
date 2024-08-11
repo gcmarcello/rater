0afaf529408a9b97a54225901725ed1cdadca528
gcmarcello <marcellogcoelho@outlook.com>
2024-08-11 02:26:41 +0000

    refactor: Add CRUD operations for fetching, deleting, and updating data in fetcher.ts

--- src/app/libs/swr/fetcher.ts
+++ src/app/libs/swr/fetcher.ts
@@ -15,3 +15,23 @@ export async function mutator<T>(url: string, { arg }: { arg: T }) {
     return parsedResponse;
   });
 }
+
+export const deleter = (url: string) =>
+  fetch(url, {
+    method: "DELETE",
+  }).then(async (r) => {
+    const parsedResponse = await r.json();
+    if (!r.ok) throw parsedResponse;
+    return parsedResponse;
+  });
+
+export async function updater<T>(url: string, { arg }: { arg: T }) {
+  return fetch(url, {
+    method: "PUT",
+    body: JSON.stringify(arg),
+  }).then(async (r) => {
+    const parsedResponse = await r.json();
+    if (!r.ok) throw parsedResponse;
+    return parsedResponse;
+  });
+}
