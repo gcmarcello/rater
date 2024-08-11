1734289e4e47283ddb9e7db6fe273d0758c48f61
gcmarcello <marcellogcoelho@outlook.com>
2024-08-11 02:27:19 +0000

    refactor: Update updateUserDto to include realName field

--- src/app/api/(resources)/users/dto.ts
+++ src/app/api/(resources)/users/dto.ts
@@ -1,8 +1,9 @@
 import { z } from "zod";
 
 export const updateUserDto = z.object({
-  email: z.string().email().optional(),
-  name: z.string().optional(),
+  email: z.string().email(),
+  name: z.string(),
+  realName: z.string().optional(),
   password: z
     .string()
     .min(6, { message: "A senha deve conter ao menos 6 caracteres." })
