928d461cccf78019f5633d4f83edd8f04f88cee9
gcmarcello <marcellogcoelho@outlook.com>
2024-08-12 21:00:37 +0000

    fix: Add padding to Container component for better spacing

--- src/app/(frontend)/components/Container.tsx
+++ src/app/(frontend)/components/Container.tsx
@@ -4,6 +4,11 @@ import styled from "styled-components";
 const Container = styled.div`
   width: 100dvw;
   height: calc(100dvh - 90px);
+  padding: 0 12px 12px 12px;
+
+  @media screen and (min-width: 1024px) {
+    padding: 0 24px 24px 24px;
+  }
 `;
 
 export default function MainContainer({
