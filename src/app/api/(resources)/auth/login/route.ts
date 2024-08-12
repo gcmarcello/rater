5cb75f73313e8af0959bb070eb8d46f6df68d3e2
gcmarcello <marcellogcoelho@outlook.com>
2024-08-12 23:06:06 +0000

    fix: replace service class for controller in auth route handlers

--- src/app/api/(resources)/auth/login/route.ts
+++ src/app/api/(resources)/auth/login/route.ts
@@ -1,11 +1,11 @@
 import { ParsedRequest } from "../../../../types/Request";
+import { AuthController } from "../controller";
 import { LoginDto } from "../dto";
-import { AuthService } from "../service";
 import { ServerResponse } from "@/app/api/classes/ServerResponse";
 
 export async function POST(request: ParsedRequest<LoginDto>) {
   try {
-    const login = await AuthService.login(request);
+    const login = await AuthController.login(request);
     return ServerResponse.json(login);
   } catch (error) {
     return ServerResponse.err(error);
--- src/app/api/(resources)/auth/signup/route.ts
+++ src/app/api/(resources)/auth/signup/route.ts
@@ -1,11 +1,11 @@
 import { ParsedRequest } from "../../../../types/Request";
+import { AuthController } from "../controller";
 import { SignupDto } from "../dto";
-import { AuthService } from "../service";
 import { ServerResponse } from "@/app/api/classes/ServerResponse";
 
 export async function POST(request: ParsedRequest<SignupDto>) {
   try {
-    const signup = await AuthService.signup(request);
+    const signup = await AuthController.signup(request);
     return ServerResponse.json(signup);
   } catch (error) {
     return ServerResponse.err(error);
