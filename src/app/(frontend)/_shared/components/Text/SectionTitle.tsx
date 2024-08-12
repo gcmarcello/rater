465a425e1f79eefa27e37b358018ea9e7be64839
gcmarcello <marcellogcoelho@outlook.com>
2024-08-12 21:00:47 +0000

    feat: Update SectionTitle component to include justify-content

--- src/app/(frontend)/_shared/components/Text/SectionTitle.tsx
+++ src/app/(frontend)/_shared/components/Text/SectionTitle.tsx
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 import Text, { TextProps } from "../Text";
+import React from "react";
 
-const StyledSectionTitle = styled(Text)`
+const StyledSectionTitle = styled.div`
   display: flex;
   align-items: center;
+  justify-content: space-between;
   border: 4px solid;
   border-top: 0;
   border-right: 0;
@@ -12,8 +14,11 @@ const StyledSectionTitle = styled(Text)`
   border-radius: 2px;
   padding-left: 8px;
   flex-grow: 1;
+  border-color: #eeeeee;
 `;
 
-export default function SectionTitle(props: TextProps) {
+export default function SectionTitle(
+  props: React.HTMLAttributes<HTMLDivElement>
+) {
   return <StyledSectionTitle {...props} />;
 }
