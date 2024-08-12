ce0848f3baf2866eab7e86f2de7bb1486e04ae00
gcmarcello <marcellogcoelho@outlook.com>
2024-08-12 23:10:13 +0000

    fix: move trycatch to controller

--- src/app/api/(resources)/auth/login/route.ts
+++ src/app/api/(resources)/auth/login/route.ts
@@ -4,10 +4,5 @@ import { LoginDto } from "../dto";
 import { ServerResponse } from "@/app/api/classes/ServerResponse";
 
 export async function POST(request: ParsedRequest<LoginDto>) {
-  try {
-    const login = await AuthController.login(request);
-    return ServerResponse.json(login);
-  } catch (error) {
-    return ServerResponse.err(error);
-  }
+  return await AuthController.login(request);
 }
--- src/app/api/(resources)/auth/signup/route.ts
+++ src/app/api/(resources)/auth/signup/route.ts
@@ -4,10 +4,5 @@ import { SignupDto } from "../dto";
 import { ServerResponse } from "@/app/api/classes/ServerResponse";
 
 export async function POST(request: ParsedRequest<SignupDto>) {
-  try {
-    const signup = await AuthController.signup(request);
-    return ServerResponse.json(signup);
-  } catch (error) {
-    return ServerResponse.err(error);
-  }
+  return await AuthController.signup(request);
 }
