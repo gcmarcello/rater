ea733b04f772d04cadfec0a6ab7e51ffba976893
gcmarcello <marcellogcoelho@outlook.com>
2024-08-12 21:01:35 +0000

    style: Remove unnecessary padding in MainPageContainer component

--- src/app/(frontend)/components/MainPageContainer.tsx
+++ src/app/(frontend)/components/MainPageContainer.tsx
@@ -2,10 +2,8 @@ import styled from "styled-components";
 
 const MainPageContainer = styled.div`
   display: grid;
-
   row-gap: 24px;
   column-gap: 0;
-  padding: 0 24px 24px 24px;
   grid-template-columns: repeat(1, minmax(0, 1fr));
 
   @media screen and (min-width: 1024px) {
