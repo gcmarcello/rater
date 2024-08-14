1fea35e46ce7bde8fa955b55a9decc5f886b7edc
gcmarcello <marcellogcoelho@outlook.com>
2024-08-14 20:34:49 +0000

    feat: Add services for managing public entities

--- src/app/api/(resources)/celebrities/service.ts
+++ src/app/api/(resources)/celebrities/service.ts
@@ -1,6 +1,41 @@
 import { Prisma } from "@prisma/client";
 import prisma from "../../infrastructure/prisma";
+import { parseNumber } from "../../utils/parseFloat";
+import {
+  CreateCelebrityDto,
+  ReadCelebrityDto,
+  UpdateCelebrityDto,
+} from "./dto";
 export class CelebrityService {
+  // CRUD
+  async createCelebrity(data: CreateCelebrityDto) {
+    return await prisma.celebrity.create(data);
+  }
+
+  async readCelebrity(data: ReadCelebrityDto) {
+    return await prisma.celebrity.findMany({
+      ...data,
+      take: data.take ? Math.min(data.take, 100) : 10,
+    });
+  }
+
+  async updateCelebrity(
+    data: UpdateCelebrityDto,
+    celebrityId: string | number
+  ) {
+    return await prisma.celebrity.update({
+      where: { id: parseNumber(celebrityId) },
+      data,
+    });
+  }
+
+  async deleteCelebrity(celebrityId: string) {
+    return await prisma.celebrity.delete({
+      where: { id: parseNumber(celebrityId) },
+    });
+  }
+
+  //SPECIAL QUERIES
   async getCelebrityById(id: string) {
     const parsedInt = parseInt(id);
     if (isNaN(parsedInt)) {
---
+++ src/app/api/(resources)/crew/service.ts
@@ -0,0 +1,51 @@
+import { Prisma } from "@prisma/client";
+import prisma from "../../infrastructure/prisma";
+import { parseNumber } from "../../utils/parseFloat";
+import { CrewCreateArgsSchema } from "../../../../../prisma/generated/zod";
+import { CreateCrewDto, ReadCrewDto, UpdateCrewDto } from "./dto";
+
+export class CrewService {
+  // CRUD
+  async createCrew(data: CreateCrewDto) {
+    return await prisma.crew.create(data);
+  }
+
+  async readCrew(data: ReadCrewDto) {
+    return await prisma.crew.findMany(data);
+  }
+
+  async updateCrew(data: UpdateCrewDto, crewId: string | number) {
+    return await prisma.crew.update({
+      where: { id: parseNumber(crewId) },
+      data: data.data,
+    });
+  }
+
+  async deleteCrew(crewId: string) {
+    return await prisma.crew.delete({
+      where: { id: parseNumber(crewId) },
+    });
+  }
+
+  // SPECIAL QUERIES
+  async readCrewById(crewId: string | number) {
+    return await prisma.crew.findUnique({
+      where: { id: parseNumber(crewId) },
+      include: { celebrity: true },
+    });
+  }
+
+  async readMovieCrew(movieId: string | number) {
+    return await prisma.crew.findMany({
+      where: { movieId: parseNumber(movieId) },
+      include: { celebrity: true },
+    });
+  }
+
+  async readShowCrew(showId: string | number) {
+    return await prisma.crew.findMany({
+      where: { showId: parseNumber(showId) },
+      include: { celebrity: true },
+    });
+  }
+}
