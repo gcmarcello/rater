b98cafef626ef15588da54779eb3d6e09e8f2b30
gcmarcello <marcellogcoelho@outlook.com>
2024-08-14 20:35:52 +0000

    feat: shared media resource

---
+++ src/app/api/(resources)/media/movie/[id]/route.ts
@@ -0,0 +1,15 @@
+import { MediaService } from "@/app/api/(resources)/media/service";
+import { response, routeHandler } from "@/app/api/handler";
+
+class MediaRoutes {
+  private mediaService: MediaService;
+  constructor() {
+    this.mediaService = new MediaService();
+  }
+
+  async GET(request: Request, { params }: { params: { id: string } }) {
+    return response(this.mediaService.similarMediaToMovie(params.id));
+  }
+}
+
+export const { GET } = routeHandler(MediaRoutes);
