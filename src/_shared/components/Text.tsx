a91e308ed8a19caeaedb9f3a918edc704c4fb7c0
gcmarcello <marcellogcoelho@outlook.com>
2024-08-15 10:36:55 +0000

    feat: Add gray variant to Text component

--- src/_shared/components/Text.tsx
+++ src/_shared/components/Text.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 export type TextProps = {
   size?: number;
-  $variant?: "white" | "dark";
+  $variant?: "white" | "dark" | "gray";
   $weight?: 300 | 400 | 500 | 600 | 700;
   $color?: string;
   children: React.ReactNode;
@@ -37,12 +37,28 @@ const DarkAnchor = styled.a<TextProps>`
   line-height: 20px;
 `;
 
+const GrayText = styled.p<TextProps>`
+  color: ${(props) => props.$color ?? "rgba(180, 180, 180, 1)"};
+  font-weight: ${(props) => props.$weight ?? 600};
+  font-size: ${(props) => `${props.size}px`};
+  line-height: 20px;
+`;
+
+const GrayAnchor = styled.a<TextProps>`
+  color: ${(props) => props.$color ?? "rgba(180, 180, 180, 1)"};
+  font-weight: ${(props) => props.$weight ?? 600};
+  font-size: ${(props) => `${props.size}px`};
+  line-height: 20px;
+`;
+
 export default function Text(props: TextProps) {
   switch (props.$variant) {
     case "white":
       return props.href ? <WhiteAnchor {...props} /> : <WhiteText {...props} />;
     case "dark":
       return props.href ? <DarkAnchor {...props} /> : <DarkText {...props} />;
+    case "gray":
+      return props.href ? <GrayAnchor {...props} /> : <GrayText {...props} />;
     default:
       return props.href ? <WhiteAnchor {...props} /> : <WhiteText {...props} />;
   }
